fb45c17fa7d8da0c79c8e18b65579d32af3cb19f
Kuwuamenu-Kofi-Gabriel <jkuwuamenu@gmail.com>
2024-03-10 12:52:00 +0000

    feat: implement splash screen as on figma

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'dart:async';
+
 import 'package:ezy_transport/routes/app_router.dart';
 import 'package:ezy_transport/routes/my_route_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 //import "package:device_preview/device_preview.dart"
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   runApp(
     EzyTransport(),
   );
+  Timer(const Duration(seconds: 3), () {
+    FlutterNativeSplash.remove();
+  });
 }
 
 class EzyTransport extends StatelessWidget {
