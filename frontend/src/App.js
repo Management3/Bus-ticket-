134b36634b52a7e3d1b33479b5f231338b45c93c
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-27 15:00:51 +0000

    noe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Hero childElement={<Login></Login>}></Hero>
+      <Footer></Footer>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern':
+          "linear-gradient(to right, rgba(44, 156, 254, .8), rgba(73, 80, 255, .8)), url('https://plus.unsplash.com/premium_photo-1661963542752-9a8a1d72fb28?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGJ1c2VzfGVufDB8fDB8fHww')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
