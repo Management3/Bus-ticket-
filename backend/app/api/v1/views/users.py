86357994e3af1d15805d55b805e643864d164dcd
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-28 17:39:38 +0000

    Created: the user routes to the basic functionality

--- backend/app/api/v1/views/users.py
+++ backend/app/api/v1/views/users.py
@@ -36,3 +36,34 @@ def delete_user(user_id):
     storage.save()
     return make_response(jsonify({}), 200)
 
+@app_views.route('/users',methods=['POST'],strict_slashes=False)
+def create_user():
+    """Creates a new user."""
+    if not request.get_json():
+        abort(400, description='Not a JSON')
+    if 'email' not in request.get_json():
+        abort(400, description="Missing Field Email")
+    if 'password' not in request.get_json():
+        abort(400, description='Missing Field Password')
+    data = request.get_json()
+    instance = User(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+@app_views.route('/users/<user_id>', methods=['PUT'],strict_slashes=False)
+def update_user(user_id):
+    """Updates a user info."""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    if not request.get_json():
+        abort(400, description='Not in JSON format!')
+    
+    fields = ['id', 'email', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in fields:
+            setattr(user, key, value)
+    storage.save()
+    return make_response(jsonify(user.to_dict()),200)
